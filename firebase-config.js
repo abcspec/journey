8184628499afdfd96cc9a12a82cc978bc19cf595
abcspec <anton.buzhynskyi@gmail.com>
2025-11-28 17:47:18 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLL0XdOi1FuBPipMcTZVokIijcgYw--wU",
+  authDomain: "screen-database-442ea.firebaseapp.com",
+  projectId: "screen-database-442ea",
+  storageBucket: "screen-database-442ea.firebasestorage.app",
+  messagingSenderId: "1084761910892",
+  appId: "1:1084761910892:web:e812c4aced9ba11923b249",
+  measurementId: "G-ZZ9Z2Q3620"
+};
